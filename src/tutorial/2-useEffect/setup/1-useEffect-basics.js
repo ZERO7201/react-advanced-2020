a695137c42461bee7d749590ee7582cf3c92f7d2
ZERO2711 <rishavsaha91@gmail.com>
2022-06-09 11:06:29 +0530

    useEffect Conditional

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,20 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log('Call useEffect');
+    if(value > 0)
+      document.title = `New Messages(${value})`;
+  })
+  return (
+  <>
+    <h1>{value}</h1>
+    <button onClick={() => setValue(value + 1)} className="btn">
+      Click Me
+    </button>
+  </>
+  );
 };
 
 export default UseEffectBasics;
